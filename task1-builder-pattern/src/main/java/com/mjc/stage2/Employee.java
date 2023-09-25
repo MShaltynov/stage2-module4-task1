aaee39565163a918323e0aed022038689f530be8
Mikhail <shaltynovm@gmail.com>
2023-09-25 14:14:19 +0530

    initial commit

--- task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
+++ task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
@@ -3,8 +3,7 @@ package com.mjc.stage2;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-@EqualsAndHashCode
-@ToString
+
 public class Employee {
     private String name;
     private String lastName;
@@ -69,6 +68,63 @@ public class Employee {
     }
 
     public static class EmployeeBuilder {
-        // Write your code here!
+        private String name;
+        private String lastName;
+        private String position;
+        private String phone;
+        private String email;
+        private String carNumber;
+        public EmployeeBuilder setName(String name){
+            this.name=name;
+            return this;
+        }
+
+        public EmployeeBuilder setLastName(String lastName){
+            this.lastName=lastName;
+            return this;
+        }
+
+        public EmployeeBuilder setPosition(String position){
+            this.position=position;
+            return this;
+        }
+        public EmployeeBuilder setPhone(String phone){
+            this.phone=phone;
+            return  this;
+        }
+        public EmployeeBuilder setEmail(String email){
+            this.email=email;
+            return this;
+        }
+
+        public EmployeeBuilder setCarNumber(String carNumber){
+            this.carNumber=carNumber;
+            return this;
+        }
+
+        public  Employee build(){
+            Employee employee = new Employee();
+            employee.name=this.name;
+            employee.lastName=this.lastName;
+            employee.position=this.lastName;
+            employee.phone=this.phone;
+            employee.email=this.email;
+            employee.carNumber=this.carNumber;
+            return employee;
+        }
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", position='" + position + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", carNumber='" + carNumber + '\'' +
+                '}';
     }
 }
--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,30 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+    @Override
+    public Connection createConnection() {
+            try {
+                Properties properties = new Properties();
+                properties.load(H2ConnectionFactory.class.getClassLoader().getResourceAsStream("h2database.properties"));
+                String url = properties.getProperty("db_url");
+                String password = properties.getProperty("password");
+                String name = properties.getProperty("user");
+                return DriverManager.getConnection(url, name, password);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            catch (IOException e){
+                throw  new RuntimeException(e);
+            }
+        }
     // Write your code here!
 }
 
